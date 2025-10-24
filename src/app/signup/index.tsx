8515d480c6f808b50a185c27a8425ada2ca761a1
emilyyvii <emilyvitoria34silva@gmail.com>
2025-10-24 15:08:06 -0300

    components

---
+++ src/app/signup/index.tsx
@@ -0,0 +1,14 @@
+import { Image, View } from "react-native";
+
+
+
+export default function Cadastro(){
+    return(
+        <View>
+        <Image source={require('@/assets/imagens/2.png')}/>
+        <View>
+           
+        </View>
+        </View>
+    )
+}
